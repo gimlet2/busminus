2ebd5dff2ebdf09af82e0978c9fa2e2862249971
gimlet <andrey@lionhearth.com>
2012-07-25 08:38:22 -0600

    templates was added.

--- main.py
+++ main.py
@@ -15,10 +15,20 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        params = {
+            'title': 'Main Page',
+            'text': 'Hello world'
+        }
+        template = jinja_environment.get_template('templates/index.html')
+        self.response.out.write(template.render(params))
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
-                              debug=True)
+    debug=True)
